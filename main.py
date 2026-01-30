57096acd4ab742f74dc1fb40d36d16c69bfc9ee7
Louis Cristante <louis.cristante@etu.minesparis.psl.eu>
2026-01-30 10:21:53 +0100

    Début de classe Spring
    (initialisation + calcul de position/angle)

---
+++ main.py
@@ -0,0 +1,23 @@
+import arcade
+import numpy as np
+
+def dist(a,b):
+    return ((a[0] - b[0])**2 + (a[0] - b[0])**2)**0.5
+
+class Spring(arcade.Sprite):
+    def __init__(self, goos):
+        self.goos = goos
+        self.l0 = dist((self.goos[0].center_x, self.goos[0].center_y), (self.goos[0].center_x, selfgoos[1].center_y))
+        self.k = 100
+        self.l = self.l0
+
+    def goos_pos(self):
+        return [np.array([goo.center_x, goo.center_y]) for goo in self.goos]
+
+    def calcul_pos(self):
+        pos = self.goos_pos()
+
+        self.l = dist(pos[0], pos[1])
+        self.image_width = self.l
+        self.center_x, self.center_y = (pos[0]+pos[1])/2
+        self.angle = np.degrees(np.arctan2(pos[1][1]-pos[0][1], pos[1][0]-pos[0][0]))
